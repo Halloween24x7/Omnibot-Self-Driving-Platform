593a6f6ed61670becaa3a90b56aee022300e4815
Halloween24x7 <61848429+Halloween24x7@users.noreply.github.com>
2020-06-10 20:23:47 -0400

    Add files via upload

--- Omnibot-Self-Driving-Platform-Arduino.ino
+++ Omnibot-Self-Driving-Platform-Arduino.ino
@@ -1,5 +1,30 @@
 // Omnibot or Equivelant platform for obstacle avoidance using an arduino, motor controller and lidar sensors
-// Some code comes from KB3HXA obstacle avoidance code which uses sonic sensors.
+
+/* Variables that may be changed based on your Bot's exact configuration */
+// Set Motor Driver Speed.
+int ABSMax = 235; // Left Wheels - 255 is Max Speed (Separate left and right allows for trimming to keep bot running in a straight line)
+int BBSMax = 255; // Right Wheels
+
+// Distances to turn or stop at. This will need to be changed depending on sensor placement.
+int MinCenterDistance = 300; // Minimum Distance before taking action (mm)
+int MinLRDistance = 300; // Minimun Distance from left and right sensors before taking action.
+int StopDistance = 200; // Distance from object to stop (mm)
+
+/* Less likely to Adjust */
+// Speed Control Multiplier
+float FifthGear = 1;
+float FourthGear = 0.90;
+float ThirdGear = 0.80;
+float SecondGear = 0.50;
+float FirstGear = 0.30;
+
+// Gear Change Distances (mm) any of the front sensors triggers a gear shift
+float FifthDistance = 1000;
+float FourthDistance = 800;
+float ThirdDistance = 500;
+float SecondDistance = 300;
+float FirstDistance = 200;
+/* End Variables that may be changed based on your Bot's exact configuration */
 
 #include <Wire.h>
 #include <VL53L0X.h>
@@ -11,102 +36,71 @@ extern "C" {
 #define TCAADDR 0x70
 #define NUM_SENSORS 3
 
-// Set up motor driver I/O
-
-int ENA = 9; // Must be a PWM pin for speed control
+// Motor Control
+int EnA = 9; // Must be a PWM pin for speed control
 int in1 = 10;
 int in2 = 11;
 
-int ENB = 5; // Must be a PWM pin for speed control
+int EnB = 5; // Must be a PWM pin for speed control
 int in3 = 6;
 int in4 = 7;
 
-// Set motor driver speed.
-// Added BBS because one of my motors ran slightly faster than the other. This allows trimming so the bot can drive in a straight line.
-// It would be better to trim using an analog input, however, we've used all of them on the UNO.
-
-int ABSMax = 235; // Left Wheels
-int BBSMax = 255; // Right Wheels
-
-int ABS = 90; // Left Wheels
-int BBS = 105; // Right Wheels
-
-// Distances to turn or stop at. This will need to be changed depending on sensor placement.
-int MinDistance = 300; // Minimum Distance before taking action (mm)
-int StopDistance = 200; // Distance from object to stop (mm)
+int ABS = 0; // Left Wheels Running Speed
+int BBS = 0; // Right Wheels Running Speed
 
 // Sensors
 int RightDistance = 0;
 int LeftDistance = 0;
 int CenterDistance = 0;
 
-// Speed Control
-float FifthGear = 1;
-float FourthGear = 0.90;
-float ThirdGear = 0.80;
-float SecondGear = 0.50;
-float FirstGear = 0.30;
-
-float FifthDistance = 1000;
-float FourthDistance = 800;
-float ThirdDistance = 500;
-float SecondDistance = 300;
-float FirstDistance = 200;
-
-void MoveBack()
+void MoveTurnAround(int ABS, int BBS)
 {
- analogWrite(ENA,ABS);
- analogWrite(ENB,BBS);
- digitalWrite(in1,HIGH);
- digitalWrite(in2,LOW);
-  
- digitalWrite(in3,HIGH);
- digitalWrite(in2,LOW);
- 
- Serial.println("Reverse!");
+  analogWrite(EnA,ABS); analogWrite(EnB,BBS);
+  digitalWrite(in1,HIGH); digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW); digitalWrite(in4,HIGH);
+  Serial.println("Turn Around!");
+  delay(100);
 }
 
-void MoveForward()
+void MoveBack(int ABS, int BBS)
 {
- analogWrite(ENA,ABS);
- analogWrite(ENB,BBS);
- digitalWrite(in1,LOW);
- digitalWrite(in2,HIGH);
- digitalWrite(in3,LOW);
- digitalWrite(in4,HIGH);
-
- Serial.println("Forward!");
+  analogWrite(EnA,ABS); analogWrite(EnB,BBS);
+  digitalWrite(in1,HIGH); digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH); digitalWrite(in2,LOW);
+  Serial.println("Reverse!");
 }
 
-void MoveLeft()
+void MoveForward(int ABS, int BBS)
 {
- analogWrite(ENA,ABS);
- analogWrite(ENB,BBS);
- digitalWrite(in1,HIGH);
- digitalWrite(in2,LOW);
- digitalWrite(in3,LOW);
- digitalWrite(in4,HIGH);
- Serial.println("Turn Left!");
+  analogWrite(EnA,ABS); analogWrite(EnB,BBS);
+  digitalWrite(in1,LOW); digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW); digitalWrite(in4,HIGH);
+  Serial.println("Forward!");
 }
 
-void MoveRight()
+void MoveLeft(int ABS, int BBS)
 {
- analogWrite(ENA,ABS);
- analogWrite(ENB,BBS);
- digitalWrite(in1,LOW);
- digitalWrite(in2,HIGH);
- digitalWrite(in3,HIGH);
- digitalWrite(in4,LOW);
- Serial.println("Turn Right!");
+  analogWrite(EnA,ABS); analogWrite(EnB,BBS);
+  digitalWrite(in1,HIGH); digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW); digitalWrite(in4,HIGH);
+  Serial.println("Turn Left!");
+}
+
+void MoveRight(int ABS, int BBS)
+{
+  analogWrite(EnA,ABS); analogWrite(EnB,BBS);
+  digitalWrite(in1,LOW); digitalWrite(in2,HIGH);
+  digitalWrite(in3,HIGH); digitalWrite(in4,LOW);
+  Serial.println("Turn Right!");
 } 
 
-  void MoveStop()
+  void MoveStop(int ABS, int BBS)
 {
-  digitalWrite(ENA,LOW);
-  digitalWrite(ENB,LOW);
+  analogWrite(EnA,0); analogWrite(EnB,0);
+//  digitalWrite(in1,LOW); digitalWrite(in2,LOW);
+//  digitalWrite(in3,LOW); digitalWrite(in4,LOW);
   Serial.println("Stop!");
 } 
-
 
 // Check LIDAR Sensors
 void tcaselect(uint8_t i) {
@@ -127,14 +121,13 @@ void setup() {
 
   Serial.println("starting...");
 
-  pinMode(ENA,OUTPUT);
+  pinMode(EnA,OUTPUT);
   pinMode(in1,OUTPUT);
   pinMode(in2,OUTPUT);
 
-  pinMode(ENB,OUTPUT);
+  pinMode(EnB,OUTPUT);
   pinMode(in3,OUTPUT);
   pinMode(in4,OUTPUT);
-  
 
   for (uint8_t i = 0; i < NUM_SENSORS; i++)
   {
@@ -150,102 +143,76 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Let's Go...");
+  
   for (uint8_t i = 0; i < NUM_SENSORS; i++)
   {
     tcaselect(i);
 
     VL53L0XPtr sensor = sensors[i];
 
-    Serial.print(i);
-    Serial.print("(");
-    Serial.print(sensor->last_status);
-    Serial.print(") => ");
+//    Serial.print(i);
+//    Serial.print("(");
+//    Serial.print(sensor->last_status);
+//    Serial.print(") => ");
 
     if (sensor->last_status == 0)
     {
-      Serial.print(sensor->readRangeContinuousMillimeters());
-      Serial.print(" mm");
+//      Serial.print(sensor->readRangeContinuousMillimeters());
+//      Serial.print(" mm");
 
       if(i==0)
       {
         LeftDistance = sensor->readRangeContinuousMillimeters();
       }
-            if(i==1)
+      if(i==1)
       {
         CenterDistance = sensor->readRangeContinuousMillimeters();
-//        if ((CenterDistance >= FifthDistance)) { ABS = ABSMax*FifthGear; BBS = BBSMax*FifthGear; Serial.println("Fifth Gear"); }
-//        else if ((CenterDistance >= FourthDistance)) { ABS = ABSMax*FourthGear; BBS = BBSMax*FourthGear; Serial.println("Fourth Gear"); }
-//        else if ((CenterDistance >= ThirdDistance)) { ABS = ABSMax*ThirdGear; BBS = BBSMax*ThirdGear; Serial.println("Third Gear"); }
-//        else if ((CenterDistance >= SecondDistance)) { ABS = ABSMax*SecondGear; BBS = BBSMax*SecondGear; Serial.println("Second Gear"); }
-//        else if ((CenterDistance >= FirstDistance)) { ABS = ABSMax*FirstGear; BBS = BBSMax*FirstGear; Serial.println("First Gear"); }
-//        else { ABS = 0; BBS = 0;  Serial.println("Nuetral");}
-
       }
-            if(i==2)
+      if(i==2)
       {
         RightDistance = sensor->readRangeContinuousMillimeters();
       }
-
-
-          
     }
 
-    if (sensor->timeoutOccurred()) {
-      Serial.print(" [timeout]");
+    if (sensor->timeoutOccurred())
+    {
+//      Serial.print(" [timeout]");
     }
-    Serial.print(";\t");  
+//    Serial.print(";\t");  
   }
+//  Serial.println();
 
-  Serial.println();
-
-/*        if ((CenterDistance >= FifthDistance)) { ABS = ABSMax*FifthGear; BBS = BBSMax*FifthGear; Serial.println("Fifth Gear"); }
-        else if ((CenterDistance >= FourthDistance)) { ABS = ABSMax*FourthGear; BBS = BBSMax*FourthGear; Serial.println("Fourth Gear"); }
-        else if ((CenterDistance >= ThirdDistance)) { ABS = ABSMax*ThirdGear; BBS = BBSMax*ThirdGear; Serial.println("Third Gear"); }
-        else if ((CenterDistance >= SecondDistance)) { ABS = ABSMax*SecondGear; BBS = BBSMax*SecondGear; Serial.println("Second Gear"); }
-        else if ((CenterDistance >= FirstDistance)) { ABS = ABSMax*FirstGear; BBS = BBSMax*FirstGear; Serial.println("First Gear"); }*/
-
-/*
-        if ((LeftDistance >= FifthDistance) || (CenterDistance >= FifthDistance) || (RightDistance >= FifthDistance)) { ABS = ABSMax*FifthGear; BBS = BBSMax*FifthGear; Serial.println("Fifth Gear"); }
-        else if ((LeftDistance >= FourthDistance) || (CenterDistance >= FourthDistance) || (RightDistance >= FourthDistance)) { ABS = ABSMax*FourthGear; BBS = BBSMax*FourthGear; Serial.println("Fourth Gear"); }
-        else if ((LeftDistance >= ThirdDistance) || (CenterDistance >= ThirdDistance) || (RightDistance >= ThirdDistance)) { ABS = ABSMax*ThirdGear; BBS = BBSMax*ThirdGear; Serial.println("Third Gear"); }
-        else if ((LeftDistance >= SecondDistance) || (CenterDistance >= SecondDistance) || (RightDistance >= SecondDistance)) { ABS = ABSMax*SecondGear; BBS = BBSMax*SecondGear; Serial.println("Second Gear"); }
-        else if ((LeftDistance >= FirstDistance) || (CenterDistance >= FirstDistance) || (RightDistance >= FirstDistance)) { ABS = ABSMax*FirstGear; BBS = BBSMax*FirstGear; Serial.println("First Gear"); }
-*/
-
-  delay(100);
-        if ((LeftDistance < FirstDistance) || (CenterDistance < FirstDistance) || (RightDistance < FirstDistance)) { ABS = ABSMax*FirstGear; BBS = BBSMax*FirstGear; Serial.println("First Gear"); }
-        else if ((LeftDistance < SecondDistance) || (CenterDistance < SecondDistance) || (RightDistance < SecondDistance)) { ABS = ABSMax*SecondGear; BBS = BBSMax*SecondGear; Serial.println("Second Gear"); }
-        else if ((LeftDistance < ThirdDistance) || (CenterDistance < ThirdDistance) || (RightDistance < ThirdDistance)) { ABS = ABSMax*ThirdGear; BBS = BBSMax*ThirdGear; Serial.println("Third Gear"); }
-        else if ((LeftDistance < FourthDistance) || (CenterDistance < FourthDistance) || (RightDistance < FourthDistance)) { ABS = ABSMax*FourthGear; BBS = BBSMax*FourthGear; Serial.println("Fourth Gear"); }
-        else if ((LeftDistance >= FifthDistance) || (CenterDistance >= FifthDistance) || (LeftDistance >= FifthDistance)) { ABS = ABSMax*FifthGear; BBS = BBSMax*FifthGear; Serial.println("Fifth Gear"); }
-
-//        else { ABS = 0; BBS = 0;  Serial.println("Nuetral");}
+  Serial.print("Left Distance: "); Serial.println(LeftDistance);
+  Serial.print("Center Distance: "); Serial.println(CenterDistance);
+  Serial.print("Right Distance: "); Serial.println(RightDistance);
 
 
+  if ((LeftDistance < FirstDistance) || (CenterDistance < FirstDistance) || (RightDistance < FirstDistance)) { ABS = ABSMax*FirstGear; BBS = BBSMax*FirstGear; Serial.println("First Gear"); }
+  else if ((LeftDistance < SecondDistance) || (CenterDistance < SecondDistance) || (RightDistance < SecondDistance)) { ABS = ABSMax*SecondGear; BBS = BBSMax*SecondGear; Serial.println("Second Gear"); }
+  else if ((LeftDistance < ThirdDistance) || (CenterDistance < ThirdDistance) || (RightDistance < ThirdDistance)) { ABS = ABSMax*ThirdGear; BBS = BBSMax*ThirdGear; Serial.println("Third Gear"); }
+  else if ((LeftDistance < FourthDistance) || (CenterDistance < FourthDistance) || (RightDistance < FourthDistance)) { ABS = ABSMax*FourthGear; BBS = BBSMax*FourthGear; Serial.println("Fourth Gear"); }
+  else if ((LeftDistance >= FifthDistance) || (CenterDistance >= FifthDistance) || (LeftDistance >= FifthDistance)) { ABS = ABSMax*FifthGear; BBS = BBSMax*FifthGear; Serial.println("Fifth Gear"); }
 
-
-
-  // Program instructions.
-
-    if ((RightDistance >= MinDistance) && (CenterDistance >= MinDistance) && (LeftDistance >= MinDistance)) // Move Forward
-    {
-      MoveForward();
-    }
-    else if ((RightDistance < LeftDistance) || (RightDistance < MinDistance))
-    {
-      MoveLeft();
-    }
-    else if ((LeftDistance < RightDistance) || (LeftDistance < MinDistance))
-    {
-      MoveRight();
-    }
+// Drive Logic
+  {
+      if ((RightDistance >= MinLRDistance) && (CenterDistance >= MinCenterDistance) && (LeftDistance >= MinLRDistance))
+      {
+        MoveForward(ABS, BBS);
+      }
+      else if ((RightDistance < LeftDistance) || (RightDistance < MinLRDistance))
+      {
+        MoveLeft(ABS, BBS);
+      }
+      else if ((LeftDistance < RightDistance) || (LeftDistance < MinLRDistance))
+      {
+        MoveRight(ABS, BBS);
+      }
     else
     {
-      MoveStop();
-    }
+      MoveStop(ABS, BBS);
+    }  
 
-    if (CenterDistance <= StopDistance) 
-    {
-      MoveStop();
-    }
+  }
+  delay(100);
 }
